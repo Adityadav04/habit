432fe52f9fcdce849c57151e2bfa79320e0c3e71
Adityadav0412 <adityadav0412@gamil.com>
2024-12-07 16:26:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/NavBar';
-import AddHabit from './Components/AddHabit';
-import ViewWeekly from './Components/ViewWeekly';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./Components/NavBar";
+import AddHabit from "./Components/AddHabit";
+import ViewWeekly from "./Components/ViewWeekly";
 
 const App = () => {
-    const [currentPage, setCurrentPage] = useState('add'); // 'add' or 'view-weekly'
+  const [currentPage, setCurrentPage] = useState("add"); // 'add' or 'view-weekly'
 
-    const renderPage = () => {
-        switch (currentPage) {
-            case 'add':
-                return <AddHabit />;
-            case 'view-weekly':
-                return <ViewWeekly />;
-            default:
-                return <AddHabit />;
-        }
-    };
+  const renderPage = () => {
+    switch (currentPage) {
+      case "add":
+        return <AddHabit />;
+      case "view-weekly":
+        return <ViewWeekly />;
+      default:
+        return <AddHabit />;
+    }
+  };
 
-    return (
-        <>
-            <NavBar setCurrentPage={setCurrentPage} />
-            <div className="container mt-5">
-                {renderPage()}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <NavBar setCurrentPage={setCurrentPage} />
+      <div className="container mt-5">{renderPage()}</div>
+    </>
+  );
 };
 
 export default App;
